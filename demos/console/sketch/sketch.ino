0831194281c9e75050137a9c5b291859529392b9
Jeffrey Kantor <Kantor.1@nd.edu>
2018-01-04 18:32:45 -0500

    add console

---
+++ demos/console/sketch/sketch.ino
@@ -0,0 +1,33 @@
+#include <WebUSB.h>
+
+WebUSB WebUSBSerial(1, "webusb.github.io/arduino/demos");
+
+#define Serial WebUSBSerial
+
+const int ledPin = 13;
+
+void setup() {
+  while (!Serial) {
+    ;
+  }
+  Serial.begin(9600);
+  Serial.write("Sketch begins.\r\n> ");
+  Serial.flush();
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (Serial && Serial.available()) {
+    int byte = Serial.read();
+    Serial.write(byte);
+    if (byte == 'H') {
+      Serial.write("\r\nTurning LED on.");
+      digitalWrite(ledPin, HIGH);
+    } else if (byte == 'L') {
+      Serial.write("\r\nTurning LED off.");
+      digitalWrite(ledPin, LOW);
+    }
+    Serial.write("\r\n> ");
+    Serial.flush();
+  }
+}
