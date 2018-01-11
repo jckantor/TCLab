43f746f4ec90faf02361d4631915ec234033e896
Jeffrey Kantor <Kantor.1@nd.edu>
2018-01-11 04:09:12 -0500

    update to working version

--- README.md
+++ README.md
@@ -5,8 +5,15 @@ Python files and interfaces for the Process Control Temperature Control Laborato
 
 This TCLab library comprises three main elements:
 
-* An Arduino sketch that implements a simple protocol for communicating with the 
-Temperature Control Laboratory device,
-* A Python library to provide a high level interface with the device,
-* A collection of Jupyter notebooks illustrating concepts in process control. 
+* An Arduino sketch (TCLab/sketch/sketch.ino) implementing a simple protocol for communicating with the 
+Temperature Control Laboratory device.
+* A Python library (TCLab/TCLab.py) providing a high level interface with the device.
+* A collection of Jupyter notebooks (notebooks/) illustrating concepts in process control. 
+
+## Installation
+
+The 
+https://github.com/adrianmihalko/ch340g-ch34g-ch34x-mac-os-x-driver/blob/master/CH34x_Install_V1.3.pkg
+
+
 
--- TCLab/TCLab.py
+++ TCLab/TCLab.py
@@ -18,41 +18,143 @@ from IPython import display
         
 class TCLab(object):
 
-    def __init__(self, port=None, baud=9600):
-        print('Opening TCLab connection')
+    def __init__(self, port=None, baud=19200, debug=False):
+        print('Connecting to TCLab')
         if not port:
-            port = self.findPort()
+            for comport in list(list_ports.comports()):
+                if comport[2].startswith((
+                        'USB VID:PID=16D0:0613',    # Arduino Uno
+                        'USB VID:PID=1A86:7523',    # Hduino
+                        'USB VID:PID=2341:8036')):  # Arduino Leonardo
+                    break
+            else:
+                print('--- Printing Serial Ports ---')            
+                for port in list(list_ports.comports()):
+                    print(port[0] + ' ' + port[1] + ' ' + port[2])
+                raise RuntimeError('No Arduino device was found.')
+            port = comport[0]
         self.sp = serial.Serial(port=port, baudrate=baud, timeout=2)
         self.sp.flushInput()
         self.sp.flushOutput()
-        time.sleep(3)
+        self.debug = debug
+        self._log = []
+        self.send('A')
+        self.receive()
+        self.send('R1')
+        self._Q1 = float(self.receive())
+        self.send('R2')
+        self._Q2 = float(self.receive())
+        self.send('T1')
+        self._T1 = float(self.receive())
+        self.send('T2')
+        self._T2 = float(self.receive())
         if self.sp.isOpen():
-            print('TCLab connection open on port ' + port)
-        self.start()
+            print('TCLab connected on port ' + port)
+        self.tstart = time.time()
+        self.updateLog()
         
-    def findPort(self):
-        found = False
-        for comport in list(list_ports.comports()):
-            # Arduino Uno
-            if comport[2].startswith('USB VID:PID=16D0:0613'):
-                port = comport[0]
-                found = True
-            # HDuino
-            if comport[2].startswith('USB VID:PID=1A86:7523'):
-                port = comport[0]
-                found = True                
-            # Leonardo
-            if comport[2].startswith('USB VID:PID=2341:8036'):
-                port = comport[0]
-                found = True
-        if (not found):
-            print('Arduino COM port not found')
-            print('Please ensure that the USB cable is connected')
-            print('--- Printing Serial Ports ---')            
-            for port in list(serial.tools.list_ports.comports()):
-                print(port[0] + ' ' + port[1] + ' ' + port[2])
-        return port
+    def __enter__(self):
+        return self
     
+    def __exit__(self, type, value, traceback):
+        self.close()
+        return
+        
+    def close(self):
+        try:
+            self.send('X')
+            self.receive()
+            self.sp.close()
+            print('TCLab disconnected successfully.')
+        except:
+            print('Problem encountered while disconnecting from TCLab.')
+            print('Please unplug and replug TCLab.')
+        return
+
+    def send(self,msg):
+        if self.debug:
+            print('Sent: "' + msg + '"')
+        self.sp.write(msg.encode())
+        self.sp.flush()
+        
+    def receive(self):
+        msg = self.sp.readline().decode('UTF-8').replace('\r\n','')
+        if self.debug:
+            print('Return: "' + msg + '"')
+        return msg
+    
+    @property
+    def version(self):
+        self.send('V')
+        return self.receive()
+    
+    @property
+    def T1(self):
+        self.send('T1')
+        self._T1 = float(self.receive())
+        self.send('T2')
+        self._T2 = float(self.receive())
+        self.updateLog()
+        return self._T1
+    
+    @property
+    def T2(self):
+        self.send('T1')
+        self._T1 = float(self.receive())
+        self.send('T2')
+        self._T2 = float(self.receive())
+        self.updateLog()
+        return self._T2
+    
+    @property
+    def Q1(self):
+        self.send('R1')
+        self._Q1 = int(self.receive())
+        self.updateLog()
+        return self._Q1
+    
+    @Q1.setter
+    def Q1(self,pwm):
+        pwm = max(0,min(255,pwm)) 
+        self.send('Q1 ' + str(pwm))
+        self._Q1 = int(self.receive())
+        self.updateLog()
+        
+    @property
+    def Q2(self):
+        self.send('R2')
+        self._Q2 = int(self.receive())
+        self.updateLog()
+        return self._Q2
+    
+    @Q2.setter
+    def Q2(self,pwm):
+        pwm = max(0,min(255,pwm)) 
+        self.send('Q2 ' + str(pwm))
+        self._Q2 = int(self.receive())
+        self.updateLog()
+        
+    def updateLog(self):
+        tnow = time.time()-self.tstart
+        self._log.append([floor(tnow/0.1)*0.1,self._Q1,self._Q2,self._T1,self._T2])
+        
+    @property
+    def log(self):
+        df = pd.DataFrame(self._log, columns = ['Time','Q1','Q2','T1','T2'])
+        df.set_index('Time',inplace=True)
+        df = df.groupby(df.index).aggregate(np.mean)
+        df.fillna(method='ffill',inplace=True)
+        return df
+    
+    def plot(self):
+        fig, axes = plt.subplots(nrows=2, ncols=1)
+        self.log[['Q1','Q2']].plot(grid=True, kind='line', 
+                drawstyle='steps-post', ax=axes[0])
+        axes[0].set_ylabel('mV')
+        self.log[['T1','T2']].plot(grid=True, kind='line', ax=axes[1])
+        axes[1].set_ylabel('deg C')
+        plt.tight_layout()
+ 
     def initplot(self,tf=20):
         # create an empty plot, and keep the line object around
         plt.figure(figsize=(12,6))
@@ -67,10 +169,10 @@ class TCLab(object):
         plt.grid()
 
         self.ax2 = plt.subplot(2,1,2)
-        self.line_Q1, = plt.step([],[],where='post',lw=2,alpha=0.8)
-        self.line_Q2, = plt.step([],[],where='post',lw=2,alpha=0.8)
+        self.line_Q1, = plt.step(0,self.Q1,where='post',lw=2,alpha=0.8)
+        self.line_Q2, = plt.step(0,self.Q2,where='post',lw=2,alpha=0.8)
         plt.xlim(0,tf)
-        plt.ylim(-5,105)
+        plt.ylim(-5,260)
         plt.ylabel('mV')
         plt.xlabel('Seconds')
         plt.legend(['Q1','Q2'])
@@ -98,122 +200,5 @@ class TCLab(object):
             self.ax1.set_ylim(5*floor(Tmin/5), 5*ceil(Tmax/5))        
         display.clear_output(wait=True)
         display.display(plt.gcf())
-
-    def start(self):
-        self.tstart = time.time()
-        self.tlog = 0
-        self.tprev = 0
-        self._Q1 = 0
-        self._Q2 = 0
-        self._log = []
-        return self.read('A')
-
-    def stop(self):
-        return self.read('X')
-    
-    @property
-    def version(self):
-        return self.read('V')
-    
-    @property
-    def T1(self):
-        self._T1 = float(self.read('T1'))
-        self.updateLog('T1',self._T1)
-        return self._T1
-    
-    @property
-    def T2(self):
-        self._T2 = float(self.read('T2'))
-        self.updateLog('T2',self._T2)
-        return self._T2
-    
-    @property
-    def Q1(self):
-        return self._Q1
-    
-    @Q1.setter
-    def Q1(self,pwm):
-        pwm = max(0,min(255,pwm)) 
-        self._Q1 = int(self.write('Q1',pwm))
-        self.updateLog('Q1',self._Q1)
-    
-    @property
-    def Q2(self):
-        return self._Q2
-    
-    @Q2.setter
-    def Q2(self,pwm):
-        pwm = max(0,min(255,pwm)) 
-        self._Q2 = int(self.write('Q2',pwm))
-        self.updateLog('Q2',self._Q2)
         
-    @property
-    def log(self):
-        df = pd.DataFrame(self._log, columns = ['Time','var','val'])
-        for key in ['Q1','Q2','T1','T2']:
-            u = df.loc[df['var'] == key,'val']
-            df.loc[u.index,key] = u
-        df.drop('val', inplace=True, axis=1)
-        df.drop('var', inplace=True, axis=1)
-        df.set_index('Time',inplace=True)
-        df = df.groupby(df.index).aggregate(np.mean)
-        df.fillna(method='ffill',inplace=True)
-        return df
-    
-    def updateLog(self,var,val):
-        tnow = time.time()-self.tstart
-        dt = tnow - self.tlog
-        self.tlog = tnow if dt > 0.01 else self.tlog
-        self._log.append([self.tlog, var, val])
- 
-    def plot(self):
-        fig, axes = plt.subplots(nrows=2, ncols=1)
-        self.log[['Q1','Q2']].plot(grid=True, kind='line', 
-                drawstyle='steps-post', ax=axes[0])
-        axes[0].set_ylabel('mV')
-        self.log[['T1','T2']].plot(grid=True, kind='line', ax=axes[1])
-        axes[1].set_ylabel('deg C')
-        plt.tight_layout()
-    
-    def read(self,cmd):
-        cmd_str = self.build_cmd_str(cmd,'')
-        try:
-            self.sp.write(cmd_str.encode())
-            self.sp.flush()
-        except Exception:
-            return None
-        return self.sp.readline().decode('UTF-8').replace("\r\n", "")
-    
-    def write(self,cmd,pwm):       
-        cmd_str = self.build_cmd_str(cmd,(pwm,))
-        try:
-            self.sp.write(cmd_str.encode())
-            self.sp.flush()
-        except:
-            return None
-        return self.sp.readline().decode('UTF-8').replace("\r\n", "")
-    
-    def build_cmd_str(self,cmd, args=None):
-        """
-        Build a command string that can be sent to the arduino.
-    
-        Input:
-            cmd (str): the command to send to the arduino, must not
-                contain a % character
-            args (iterable): the arguments to send to the command
-        """
-        if args:
-            args = ' '.join(map(str, args))
-        else:
-            args = ''
-        return "{cmd} {args}\n".format(cmd=cmd, args=args)
-        
-    def close(self):
-        try:
-            self.sp.close()
-            print('Arduino disconnected successfully')
-        except:
-            print('Problems disconnecting from Arduino.')
-            print('Please unplug and replug Arduino.')
-        return True
-    
+             
