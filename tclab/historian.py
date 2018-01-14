8294584a134e4a7ec5b6a3ab809c04d5bcb48d11
Jeffrey Kantor <Kantor.1@nd.edu>
2018-01-14 17:06:55 -0500

    correct documentation of clock function

--- tclab/historian.py
+++ tclab/historian.py
@@ -17,7 +17,7 @@ class Historian(object):
     def __init__(self,lab):
         self.lab = lab
         self.tstart = time.time()
-        self.t = 0
+        self.tnow = 0
         self._log = [[0, self.lab.T1, self.lab.T2, self.lab.Q1(), self.lab.Q2()]]
         self.RTplot = False
     
@@ -27,18 +27,10 @@ class Historian(object):
     def __exit__(self, type, value, traceback):
         return
     
-    def update(self, t = None):
-        if t is None:
-            self.t = time.time() - self.tstart
-        else:
-            self.t = t
-        self._log.append([self.t, self.lab.T1, self.lab.T2, self.lab.Q1(), self.lab.Q2()])
-        if self.RTplot:
-            self.updateplot()
-
     def initplot(self, tperiod = 20):
         self.RTplot = True
-        self._log = [[0, self.lab.T1, self.lab.T2, self.lab.Q1(), self.lab.Q2()]]
+        self.tnow = 0
+        self._log = [[self.tnow, self.lab.T1, self.lab.T2, self.lab.Q1(), self.lab.Q2()]]
         t,T1,T2,Q1,Q2 = self._log[0]
         plt.figure(figsize=(10, 5))
         self.ax1 = plt.subplot(2, 1, 1)
@@ -55,8 +47,8 @@ class Historian(object):
         plt.grid()
 
         self.ax2 = plt.subplot(2,1,2)
-        self.line_Q1, = plt.step(0, self.lab.Q1(), where='pre', lw=2, alpha=0.8)
-        self.line_Q2, = plt.step(0, self.lab.Q2(), where='pre', lw=2, alpha=0.8)
+        self.line_Q1, = plt.step(0, Q1, where='pre', lw=2, alpha=0.8)
+        self.line_Q2, = plt.step(0, Q2, where='pre', lw=2, alpha=0.8)
         plt.xlim(0, 1.05*tperiod)
         plt.ylim(-5, 110)
         plt.ylabel('Percent of Max Power')
@@ -64,6 +56,15 @@ class Historian(object):
         plt.legend(['Q1','Q2'])
         plt.grid()
         plt.tight_layout()
+        
+    def update(self, tnow = None):
+        if tnow is None:
+            self.tnow = time.time() - self.tstart
+        else:
+            self.tnow = tnow
+        self._log.append([self.tnow, self.lab.T1, self.lab.T2, self.lab.Q1(), self.lab.Q2()])
+        if self.RTplot:
+            self.updateplot()
 
     def updateplot(self):
         [t,T1,T2,Q1,Q2] = [[row[i] for row in self._log] for i in range(5)]
@@ -75,7 +76,7 @@ class Historian(object):
         self.line_Q1.set_ydata(Q1)
         self.line_Q2.set_xdata(t)
         self.line_Q2.set_ydata(Q2)
-        if self.t > self.ax1.get_xlim()[1]:
+        if self.tnow > self.ax1.get_xlim()[1]:
             self.ax1.set_xlim(0, 1.4*self.ax1.get_xlim()[1])
             self.ax2.set_xlim(0, 1.4*self.ax2.get_xlim()[1])
         Tmax = max(max(T1), max(T2))
@@ -88,4 +89,24 @@ class Historian(object):
     @property
     def log(self):
         return self._log
+    
+    @property
+    def t(self):
+        return [row[0] for row in self._log]
+
+    @property
+    def T1(self):
+        return [row[1] for row in self._log]
+
+    @property
+    def T2(self):
+        return [row[2] for row in self._log]
+
+    @property
+    def Q1(self):
+        return [row[3] for row in self._log]
+
+    @property
+    def Q2(self):
+        return [row[4] for row in self._log]
 
