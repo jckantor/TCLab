6a2907df8cb2bae252eeecb8f6914300b148be6b
Jeffrey Kantor <Kantor.1@nd.edu>
2018-01-14 13:25:38 -0500

    changed graphic size to accommodate classroom projectors

--- tclab/historian.py
+++ tclab/historian.py
@@ -17,7 +17,8 @@ class Historian(object):
     def __init__(self,lab):
         self.lab = lab
         self.tstart = time.time()
-        self._log = [[0.0, self.lab.T1, self.lab.T2, self.lab.Q1(), self.lab.Q2()]]
+        self.t = 0
+        self._log = [[0, self.lab.T1, self.lab.T2, self.lab.Q1(), self.lab.Q2()]]
         self.RTplot = False
     
     def __enter__(self):
@@ -26,25 +27,20 @@ class Historian(object):
     def __exit__(self, type, value, traceback):
         return
     
-    def update(self):
-        self.tp = time.time() - self.tstart
-        self.T1 = self.lab.T1
-        self.T2 = self.lab.T2
-        self.Q1 = self.lab.Q1()
-        self.Q2 = self.lab.Q2()
-
-        self._log.append([self.tp, self.T1, self.T2, self.Q1, self.Q2])
+    def update(self, t = None):
+        self.t = time.time() - self.tstart
+        self._log.append([self.t, self.lab.T1, self.lab.T2, self.lab.Q1(), self.lab.Q2()])
         if self.RTplot:
             self.updateplot()
 
-    def initplot(self, tf=20):
+    def initplot(self, tperiod = 20):
         self.RTplot = True
         # create an empty plot, and keep the line object around
-        plt.figure(figsize=(12, 6))
+        plt.figure(figsize=(10, 5))
         self.ax1 = plt.subplot(2, 1, 1)
         self.line_T1, = plt.plot(0, self.lab.T1, lw=2, alpha=0.8)
         self.line_T2, = plt.plot(0, self.lab.T2, lw=2, alpha=0.8)
-        plt.xlim(0, 1.05*tf)
+        plt.xlim(0, 1.05*tperiod)
         plt.title('Temperature Control Lab')
         plt.ylabel('Temperature / °C')
         plt.xlabel('Time / Seconds')
@@ -54,9 +50,9 @@ class Historian(object):
         self.ax2 = plt.subplot(2,1,2)
         self.line_Q1, = plt.step(0, self.lab.Q1(), where='post', lw=2, alpha=0.8)
         self.line_Q2, = plt.step(0, self.lab.Q2(), where='post', lw=2, alpha=0.8)
-        plt.xlim(0, 1.05*tf)
+        plt.xlim(0, 1.05*tperiod)
         plt.ylim(-5, 110)
-        plt.ylabel('Percent of Maximum Power')
+        plt.ylabel('Percent of Max Power')
         plt.xlabel('Time / Seconds')
         plt.legend(['Q1','Q2'])
         plt.grid()
@@ -72,9 +68,9 @@ class Historian(object):
         self.line_Q1.set_ydata(Q1)
         self.line_Q2.set_xdata(t)
         self.line_Q2.set_ydata(Q2)
-        if self.tp > self.ax1.get_xlim()[1]:
-            self.ax1.set_xlim(0, 1.5*self.ax1.get_xlim()[1])
-            self.ax2.set_xlim(0, 1.5*self.ax2.get_xlim()[1])
+        if self.t > self.ax1.get_xlim()[1]:
+            self.ax1.set_xlim(0, 1.3*self.ax1.get_xlim()[1])
+            self.ax2.set_xlim(0, 1.3*self.ax2.get_xlim()[1])
         Tmax = max(max(T1), max(T2))
         Tmin = min(min(T1), min(T2))
         if (Tmax > self.ax1.get_ylim()[1]) or (Tmin < self.ax1.get_ylim()[0]):
