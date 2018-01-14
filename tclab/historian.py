d3cbe5efbfcc5a2f481db214159ff742f1f6e755
Jeffrey Kantor <Kantor.1@nd.edu>
2018-01-14 11:23:17 -0500

    attempt at backward compatibility

--- tclab/historian.py
+++ tclab/historian.py
@@ -5,6 +5,8 @@ Created on Fri Jan 12 09:14:07 2018
 
 @author: jeff
 """
+from __future__ import print_function
+from __future__ import division
 
 import time
 from math import ceil, floor
--- tclab/tclab.py
+++ tclab/tclab.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 import time
 import serial
 from serial.tools import list_ports
