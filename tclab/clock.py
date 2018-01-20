7cd7a9080f42427264270217db590432cbaf6f84
Jeffrey Kantor <Kantor.1@nd.edu>
2018-01-20 08:27:44 -0500

    change default to strict time keeping

--- tclab/clock.py
+++ tclab/clock.py
@@ -2,19 +2,20 @@ import time
 
 
 class TCLabClockError(RuntimeError):
+    """Error class to allow exception handling of clock sync errors."""
     def __init__(self, message):
         RuntimeError.__init__(self, message)
 
 
-def clock(tperiod, tstep=1, strict=False, tol=0.1):
+def clock(tperiod, tstep=1, strict=True, tol=0.1):
     start_time = time.time()
     prev_time = start_time
     fuzz = 0.004
     k = 0
     while (prev_time-start_time) <= tperiod + fuzz:
         if strict and ((prev_time - start_time) > (k*tstep + tol)):
-                raise TCLabClockError("Clock lost real time syncronization.")
-        yield round(prev_time - start_time, 2)
+                raise TCLabClockError("Clock lost real time synchronization.")
+        yield round(prev_time - start_time, 1)
         if strict:
             tsleep = (k+1)*tstep - (time.time() - start_time) - fuzz
         else:
