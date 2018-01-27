d78351d69123d2b774da1d1075d23e05f9fca612
Jeffrey Kantor <Kantor.1@nd.edu>
2018-01-27 16:32:24 -0500

    add tornado to requirements; update plotter window control

--- tclab/historian.py
+++ tclab/historian.py
@@ -47,8 +47,10 @@ class Historian(object):
 
 
 class Plotter:
-    def __init__(self, historian, tperiod=20):
+    def __init__(self, historian, twindow=120):
         self.historian = historian
+        self.twindow = twindow
+        self.tmin = 0
 
         import matplotlib.pyplot as plt
         from IPython import display
@@ -68,7 +70,7 @@ class Plotter:
             y = self.historian.at(0,[self.historian.columns[n+1]])[0]
             li, = plt.step(0, y, where='post', **line_options)          
             self.lines.append(li)
-            plt.xlim(0, 1.05 * tperiod)
+            plt.xlim(0, self.twindow)
             plt.ylim(y-2, y+2)
             plt.ylabel(self.historian.columns[n+1])
             plt.grid()
@@ -82,15 +84,20 @@ class Plotter:
         plt = self.plt
         display = self.display
 
-        t = self.historian.fields[0]
+        if self.historian.tnow > self.tmin + self.twindow:
+            self.tmin += self.twindow
+            for n in range(0, len(self.historian.columns)-1):
+                self.axes[n].set_xlim(self.tmin, self.tmin + self.twindow)
+
+        i = bisect.bisect_left(self.historian.fields[0],self.tmin)
+        t = self.historian.fields[0][i:]
         for n in range(0, len(self.historian.columns)-1):
-            y = self.historian.fields[n+1]
+            y = self.historian.fields[n+1][i:]
             self.lines[n].set_data(t, y)
-            ymax = max(y)
-            ymin = min(y)
-            if (ymax > self.axes[n].get_ylim()[1]) or (ymin < self.axes[n].get_ylim()[0]):
-                self.axes[n].set_ylim(ymin-1, ymax+2)
-            if self.historian.tnow > self.axes[n].get_xlim()[1]:
-                self.axes[n].set_xlim(0, 1.4 * self.axes[n].get_xlim()[1])
+            ymin, ymax = self.axes[n].get_ylim()
+            if max(y) > ymax:
+                self.axes[n].set_ylim(ymin, max(y) + 2)
+            if min(y) < ymin:
+                self.axes[n].set_ylim(min(y) - 2, ymax)
         display.clear_output(wait=True)
         display.display(plt.gcf())
