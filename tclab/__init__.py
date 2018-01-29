ef5c841d37c6b57dba2898bf88dad1510d5d1ca8
Carl Sandrock <carl.sandrock@up.ac.za>
2018-01-29 06:20:47 +0200

    Add docstring for `tclab.setup`

--- tclab/__init__.py
+++ tclab/__init__.py
@@ -3,6 +3,26 @@ from .clock import clock, time
 from .historian import Historian, Plotter
 
 def setup(connected=True, speedup=1):
+    """Set up a lab session with simple switching between real and model lab
+
+    The idea of this function is that you will do
+
+    >>> lab = setup(connected=True)
+
+    to obtain a TCLab class reference. If `connected=False` then you will
+    receive a TCLabModel class reference. This allows you to switch between
+    the model and the real lab in your code easily.
+
+    The speedup option can only be used when `connected=False` and is a factor
+    by which the clock will be sped up during the simulation.
+
+    For example
+
+    >>> lab = setup(connected=False, speedup=2)
+
+    will run the clock at twice real time (which means that the whole simulation
+    will take half the time it would if connected to a real device).
+    """
     from .clock import setup as clocksetup
 
     if connected:
