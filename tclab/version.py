3d86e2e61d00b82eef440b389573f050d57df162
Jeffrey Kantor <kantor.1@nd.edu>
2021-02-12 19:39:16 -0500

    Fix comments

--- tclab/version.py
+++ tclab/version.py
@@ -1,6 +1,6 @@
 # Store the version here so:
 # 1) we don't load dependencies by storing it in __init__.py
-# 2) we can import it in setup.py for the same reason
-# 3) we can import it into your module module
+# 2) we can import it in setup.py without loadiing dependencies
+# 3) we can import it into your module
 
 __version__ = "0.4.10dev"
